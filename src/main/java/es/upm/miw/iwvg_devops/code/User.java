d024ddb8b4bb5acbdefb5c73eb9bc3798d6da53d
laura <contact0faztweb.com>
2024-09-19 09:22:26 +0200

    rectified issues of sonar #19

--- src/main/java/es/upm/miw/iwvg_devops/code/User.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/User.java
@@ -3,7 +3,6 @@ package es.upm.miw.iwvg_devops.code;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class User {
     private String id;
